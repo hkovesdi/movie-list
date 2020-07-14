39044139acae849a40b9e065d382dc4218feb430
hkovesdi <kovesdi.hunor99@gmail.com>
2020-07-14 18:09:39 +0200

    Fix userMovies relationship on User

--- app/User.php
+++ app/User.php
@@ -42,6 +42,6 @@ class User extends Authenticatable
      */
     public function userMovies()
     {
-        return $this->hasMany('App\UserMovie', 'users_movies');
+        return $this->hasMany('App\UserMovie');
     }
 }
