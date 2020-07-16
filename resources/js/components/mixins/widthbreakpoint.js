bb7cdec2a33c969344f80de49e5d66e4a4ab44aa
René Lajvort <rene@renkee.eu>
2020-07-16 16:47:51 +0200

    refactor computed function into method

--- resources/js/components/mixins/widthbreakpoint.js
+++ resources/js/components/mixins/widthbreakpoint.js
@@ -2,12 +2,14 @@ export default {
   computed: {
     breakpoints() {
       return this.$vuetify.breakpoint.thresholds
-    },
-    windowWidthAboveBreakpoint() {
+    }
+  },
+  methods: {
+    windowWidthAboveBreakpoint(point) {
       // If this value is low, then some elements that should be hidden flash for a moment on first pageload.
       const defaultWidth = 9999999
 
-      return (point) => (this.$store.state.window.width || defaultWidth) >= this.breakpoints[point]
+      return (this.$store.state.window.width || defaultWidth) >= this.breakpoints[point]
     }
   }
 }
