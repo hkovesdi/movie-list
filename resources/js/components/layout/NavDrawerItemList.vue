a6ae2347e8b104ae65cf8bddedc1d42d475f5947
René Lajvort <rene@renkee.eu>
2020-07-16 16:48:31 +0200

    match routes even if queries don't match

--- resources/js/components/layout/NavDrawerItemList.vue
+++ resources/js/components/layout/NavDrawerItemList.vue
@@ -1,13 +1,6 @@
 <template>
   <div>
-    <v-list-item
-      v-for="item in items"
-      :key="item.text"
-      :to="item.to"
-      link
-      exact
-      @click="item.dispatch !== undefined ? $store.dispatch(item.dispatch) : ''"
-    >
+    <v-list-item v-for="item in items" :key="item.text" :to="item.to" :class="matchWithOrWithoutQuery(item.to)" link exact>
       <v-list-item-icon>
         <v-icon>{{ item.icon.code }}</v-icon>
       </v-list-item-icon>
@@ -26,6 +19,11 @@ export default {
     items: {
       type: Array,
       required: true
+    }
+  },
+  methods: {
+    matchWithOrWithoutQuery(path) {
+      return this.$route.path === path ? 'v-list-item--active' : ''
     }
   }
 }
