38851ea005d4ba6f8440da5f353f5aa0a97f336c
hkovesdi <kovesdi.hunor99@gmail.com>
2020-07-19 13:22:26 +0200

    create table friend requests table

---
+++ database/migrations/2020_07_19_104125_create_friend_requests_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFriendRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friend_requests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sender_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('receiver_id')->constrained('users')->onDelete('cascade');
+            $table->boolean('accepted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {   
+        Schema::table('friend_requests', function (Blueprint $table) {
+            $table->dropForeign(['sender_id']);
+            $table->dropForeign(['receiver_id']);
+        });
+        Schema::dropIfExists('friend_requests');
+    }
+}
