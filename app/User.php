751c46ee85be30c6753d91f587d0ed0644e5671c
hkovesdi <kovesdi.hunor99@gmail.com>
2020-07-21 13:21:45 +0200

    add bio field to User

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'username', 'email', 'avatar', 'password',
+        'username', 'email', 'bio', 'avatar', 'password',
     ];
 
     /**
