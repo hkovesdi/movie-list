15e27b407e180d42a9b5ff062645338a6a03fc18
René Lajvort <rene@renkee.eu>
2020-07-21 03:37:42 +0200

    homepage design experimentation

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,64 +1,43 @@
 <template>
-  <v-card class="mx-auto" max-width="400">
-    <v-list-item two-line>
-      <v-list-item-content>
-        <v-list-item-title class="headline">San Francisco</v-list-item-title>
-        <v-list-item-subtitle>Mon, 12:30 PM, Mostly sunny</v-list-item-subtitle>
-      </v-list-item-content>
-    </v-list-item>
-
-    <v-card-text>
-      <v-row align="center">
-        <v-col class="display-3" cols="6">
-          23&deg;C
-        </v-col>
-        <v-col cols="6">
-          <v-img src="https://cdn.vuetifyjs.com/images/cards/sun.png" alt="Sunny image" width="92"></v-img>
-        </v-col>
-      </v-row>
-    </v-card-text>
-
-    <v-list-item>
-      <v-list-item-icon>
-        <v-icon @click="login()">fal fa-alien</v-icon>
-      </v-list-item-icon>
-      <v-list-item-subtitle>23 km/h</v-list-item-subtitle>
-    </v-list-item>
-
-    <v-list-item>
-      <v-list-item-icon>
-        <v-icon>mdi-cloud-download</v-icon>
-      </v-list-item-icon>
-      <v-list-item-subtitle>48%</v-list-item-subtitle>
-    </v-list-item>
-
-    <v-slider v-model="time" :max="6" :tick-labels="labels" class="mx-4" ticks></v-slider>
-
-    <v-list class="transparent">
-      <v-list-item v-for="item in forecast" :key="item.day">
-        <v-list-item-title>{{ item.day }}</v-list-item-title>
-
-        <v-list-item-icon>
-          <v-icon>{{ item.icon }}</v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-subtitle class="text-right">
-          {{ item.temp }}
-        </v-list-item-subtitle>
-      </v-list-item>
-    </v-list>
-
-    <v-divider></v-divider>
-
-    <v-card-actions>
-      <v-btn text>Full Report</v-btn>
-    </v-card-actions>
-  </v-card>
+  <div>
+    <div>
+      <h1 class="font-weight-black">
+        Most recent
+      </h1>
+      <MovieShowcase />
+    </div>
+    <div>
+      <h1>
+        Highest rated
+      </h1>
+      <ul>
+        <li>Movie 1</li>
+        <li>Movie 2</li>
+        <li>Movie 3</li>
+        <li>Movie 4</li>
+      </ul>
+    </div>
+    <div>
+      <h1>
+        Really important category
+      </h1>
+      <ul>
+        <li>Movie 1</li>
+        <li>Movie 2</li>
+        <li>Movie 3</li>
+        <li>Movie 4</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
+import MovieShowcase from './movies/MovieShowcase'
 import gql from 'graphql-tag'
 export default {
+  components: {
+    MovieShowcase
+  },
   data() {
     return {
       movieId: 2,
---
+++ resources/js/components/movies/MovieShowcase.vue
@@ -0,0 +1,119 @@
+<template>
+  <div v-dragscroll.x class="outer">
+    <div class="inner">
+      <a href="#1">
+        <v-img
+          height="300"
+          contain
+          src="https://m.media-amazon.com/images/M/MV5BODhkZGE0NDQtZDc0Zi00YmQ4LWJiNmUtYTY1OGM1ODRmNGVkXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg"
+        />
+      </a>
+    </div>
+    <div class="inner">
+      <a href="#2">
+        <v-img
+          height="300"
+          contain
+          src="https://m.media-amazon.com/images/M/MV5BMGM4M2Q5N2MtNThkZS00NTc1LTk1NTItNWEyZjJjNDRmNDk5XkEyXkFqcGdeQXVyMjA0MDQ0Mjc@._V1_UX182_CR0,0,182,268_AL_.jpg"
+        />
+      </a>
+    </div>
+    <div class="inner">
+      <a href="#3">
+        <v-img
+          height="300"
+          contain
+          src="https://m.media-amazon.com/images/M/MV5BNzU3ZTI1OTktNjVkNy00OWEzLWIyNzAtMWQ5YjRkZDU1ZjAxXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_UX182_CR0,0,182,268_AL_.jpg"
+        />
+      </a>
+    </div>
+    <div class="inner">
+      <a href="#4">
+        <v-img
+          height="300"
+          contain
+          src="https://m.media-amazon.com/images/M/MV5BY2JiYTNmZTctYTQ1OC00YjU4LWEwMjYtZjkwY2Y5MDI0OTU3XkEyXkFqcGdeQXVyNTI4MzE4MDU@._V1_UX182_CR0,0,182,268_AL_.jpg"
+        />
+      </a>
+    </div>
+    <div class="inner">
+      <a href="#5">
+        <v-img
+          height="300"
+          contain
+          src="https://m.media-amazon.com/images/M/MV5BNDQ2YWVmZjEtMTE1ZS00MDk1LTg5ODUtMDVlODk0OTM2ODMwXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX182_CR0,0,182,268_AL_.jpg"
+        />
+      </a>
+    </div>
+    <div class="inner">
+      <a href="#6">
+        <v-img
+          height="300"
+          contain
+          src="https://m.media-amazon.com/images/M/MV5BNWExNzg3MmMtYjc3MS00MzFlLWJiOWQtNWYxZTgxNjhlZTQ2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UX182_CR0,0,182,268_AL_.jpg"
+        />
+      </a>
+    </div>
+    <div class="inner">
+      <a href="#7">
+        <v-img
+          height="300"
+          contain
+          src="https://m.media-amazon.com/images/M/MV5BMWEzNmUxZTMtZjY0My00OGNmLWIyNDctODM2YzZjM2YwZWEwXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UY268_CR0,0,182,268_AL_.jpg"
+        />
+      </a>
+    </div>
+    <div class="inner">
+      <a href="#8">
+        <v-img
+          height="300"
+          contain
+          src="https://m.media-amazon.com/images/M/MV5BZmE0MGJhNmYtOWNjYi00Njc5LWE2YjEtMWMxZTVmODUwMmMxXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX182_CR0,0,182,268_AL_.jpg"
+        />
+      </a>
+    </div>
+    <div class="inner">
+      <a href="#9">
+        <v-img
+          height="300"
+          contain
+          src="https://m.media-amazon.com/images/M/MV5BMTI5NmViY2YtNDk5NC00NjY2LWFlNGYtOGEwNzY1MTZmMjFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg"
+        />
+      </a>
+    </div>
+    <div class="inner">
+      <a href="#10">
+        <v-img
+          height="300"
+          contain
+          src="https://m.media-amazon.com/images/M/MV5BZTliNWJhM2YtNDc1MC00YTk1LWE2MGYtZmE4M2Y5ODdlNzQzXkEyXkFqcGdeQXVyMzY0MTE3NzU@._V1_UX182_CR0,0,182,268_AL_.jpg"
+        />
+      </a>
+    </div>
+  </div>
+</template>
+<script>
+import { dragscroll } from 'vue-dragscroll'
+export default {
+  directives: {
+    dragscroll
+  }
+}
+</script>
+
+<style lang="scss">
+.outer {
+  overflow-x: hidden;
+  overflow-y: hidden;
+  height: 326px;
+  padding: 4px;
+  display: flex;
+  justify-content: flex-start;
+  flex-flow: row nowrap;
+  .inner {
+    max-width: 200px;
+    flex: 0 0 auto;
+    margin-right: 12px;
+  }
+}
+</style>
