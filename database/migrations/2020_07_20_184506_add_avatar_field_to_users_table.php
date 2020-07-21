8dbaa39f8d55f37c57d6f57c003e7ffcde60f187
hkovesdi <kovesdi.hunor99@gmail.com>
2020-07-21 13:19:35 +0200

    Add avatar field to User

--- database/migrations/2020_07_20_184506_add_avatar_field_to_users_table.php
+++ database/migrations/2020_07_20_184506_add_avatar_field_to_users_table.php
@@ -14,7 +14,7 @@ class AddAvatarFieldToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('avatar')->nullable();
+            $table->string('avatar')->nullable()->after('email');
         });
     }
 
