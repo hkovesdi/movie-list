e02df432a223c3c8ab07a9dfbec6832eb29a72b1
René Lajvort <rene@renkee.eu>
2020-07-22 19:36:38 +0200

    remove unnecessary import

--- resources/js/plugins/vue-apollo.js
+++ resources/js/plugins/vue-apollo.js
@@ -1,5 +1,4 @@
 import { ApolloClient } from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { createUploadLink } from 'apollo-upload-client'
 import Cookies from 'js-cookie'
