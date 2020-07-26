0aab56f0d049e86db50e74e9acee38d96e25424f
René Lajvort <rene@renkee.eu>
2020-07-26 09:04:56 +0200

    experimentation with home page layout and anims

--- resources/js/components/movies/MovieShowcase.vue
+++ resources/js/components/movies/MovieShowcase.vue
@@ -1,12 +1,15 @@
 <template>
   <div v-dragscroll.x class="outer">
-    <div v-for="movie in movies" :key="movie.title" class="inner">
-      <a href="#">
-        <v-img height="300" contain :src="movie.img_url" />
-      </a>
+    <div v-for="movie in movies" :key="movie.title" style="position: relative;" class="inner">
+      <div style="height: 108px; width: 72.8px;"></div>
+      <div style="line-height: 1.3;" class="showcase-title subtitle-1 font-weight-bold mb-1 mt-2 px-1">{{ movie.title }}</div>
+      <div style="line-height: 1.3; height: 90px; overflow-y: auto;" class="showcase-desc body-2 px-1">{{ movie.description }}</div>
+      <v-btn class="showcase-button" style="position: absolute; bottom: 0; left: 0; right: 0;" outlined color="primary">Learn more</v-btn>
+      <v-img style="position: absolute; top: 0; left: 0;" class="poster" height="270" contain :src="movie.img_url" />
     </div>
   </div>
 </template>
+
 <script>
 import { dragscroll } from 'vue-dragscroll'
 export default {
@@ -32,9 +35,31 @@ export default {
   justify-content: flex-start;
   flex-flow: row nowrap;
   .inner {
-    max-width: 200px;
+    width: 182px;
+    height: 270px;
     flex: 0 0 auto;
     margin-right: 12px;
+  }
+}
+.poster {
+  transform-origin: 8px 8px;
+  transition: transform 0.3s;
+}
+.showcase-title,
+.showcase-desc,
+.showcase-button {
+  opacity: 0;
+  width: 100%;
+  transition: opacity 0.3s;
+}
+.inner:hover {
+  .poster {
+    transform: scale(0.4, 0.4);
+  }
+  .showcase-title,
+  .showcase-desc,
+  .showcase-button {
+    opacity: 1;
   }
 }
 </style>
