73d8d6caa9839fb8233c7e1ec24abc04fcda53ea
hkovesdi <kovesdi.hunor99@gmail.com>
2020-08-02 17:32:48 +0200

    Make movie.vue get it's data from a gql query if the movie object is not passed in as a prop

--- resources/js/components/movies/Movie.vue
+++ resources/js/components/movies/Movie.vue
@@ -1,13 +1,56 @@
 <template>
-  <div></div>
+  <div v-if="movie">
+    <v-img width="292" height="429" :src="movie.high_res_poster_url">
+      <template v-slot:placeholder>
+        <v-row class="fill-height ma-0" align="center" justify="center">
+          <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
+        </v-row>
+      </template>
+    </v-img>
+  </div>
 </template>
 
 <script>
+import gql from 'graphql-tag'
 export default {
   props: {
-    movie: {
+    movieProp: {
       default: null,
       type: Object
+    }
+  },
+  data: function () {
+    return {
+      movie: this.movieProp
+    }
+  },
+  apollo: {
+    movie: {
+      query: gql`
+        query movie($id: ID!) {
+          movie(id: $id) {
+            id
+            title
+            img_url
+            users_rating
+            description
+            rating
+            year
+            runtime
+            tagline
+            high_res_poster_url
+          }
+        }
+      `,
+      variables() {
+        return { id: this.$route.params.id }
+      },
+      skip() {
+        return this.movieProp !== null
+      },
+      result({ data }) {
+        if (data.movie === null) this.$router.replace({ name: 'NotFound', params: { '0': this.$route.path } })
+      }
     }
   }
 }
