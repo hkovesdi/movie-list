099db50bd6119f3b22fd5c098de84bcf3dbde4d5
hkovesdi <kovesdi.hunor99@gmail.com>
2020-08-02 21:26:35 +0200

    add skeleton loaders to movie

--- resources/js/components/movies/Movie.vue
+++ resources/js/components/movies/Movie.vue
@@ -1,12 +1,29 @@
 <template>
-  <div v-if="movie">
-    <v-img width="292" height="429" :src="movie.high_res_poster_url">
-      <template v-slot:placeholder>
-        <v-row class="fill-height ma-0" align="center" justify="center">
-          <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-        </v-row>
-      </template>
-    </v-img>
+  <div v-if="movie !== null" class="mt-10 ml-6 d-flex">
+    <div>
+      <v-img width="296" height="435" :src="movie.high_res_poster_url">
+        <template v-slot:placeholder>
+          <v-skeleton-loader class="poster-skeleton" :loading="true" width="296" height="435" type="image" />
+        </template>
+      </v-img>
+    </div>
+    <div class="ml-4" style="max-width: 55ch;">
+      <h1>{{ movie.title }}</h1>
+      <p class="text-body-2 text--secondary">{{ movie.tagline }}</p>
+      <p class="text--primary">{{ movie.description }}</p>
+    </div>
+  </div>
+  <div v-else class="mt-10 ml-6 d-flex">
+    <div class="poster-skeleton">
+      <v-skeleton-loader :loading="true" width="296" height="435" type="image" />
+    </div>
+    <div class="ml-4 mt-1" style="max-width: 55ch;">
+      <v-skeleton-loader :loading="true" type="heading" width="55ch" class="mb-4" />
+      <v-skeleton-loader :loading="true" type="text" class="mb-6" />
+      <v-skeleton-loader :loading="true" type="paragraph" />
+      <v-skeleton-loader :loading="true" type="paragraph" />
+      <v-skeleton-loader :loading="true" type="paragraph" />
+    </div>
   </div>
 </template>
 
@@ -55,3 +72,9 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+.poster-skeleton .v-skeleton-loader__image {
+  height: 100% !important;
+  width: 100% !important;
+}
+</style>
