cf5a8bf69c8c71437be1d60886635b6a5faa8b18
René Lajvort <rene@renkee.eu>
2020-08-08 22:59:48 +0200

    get user from route in profile page

--- resources/js/components/user/name/Index.vue
+++ resources/js/components/user/name/Index.vue
@@ -1,9 +1,37 @@
 <template>
-  <div>Hello {{ $route.params.name }}!</div>
+  <div>Hello {{ user.username }}!</div>
 </template>
 
 <script>
-export default {}
+import gql from 'graphql-tag'
+export default {
+  computed: {
+    uuser() {
+      return !this.$apollo.queries.user ? this.user : {}
+    }
+  },
+  apollo: {
+    user() {
+      return {
+        query: gql`
+          query user($searchString: Mixed!, $searchField: UserSearchField!) {
+            user: user(searchString: $searchString, searchField: $searchField) {
+              id
+              username
+            }
+          }
+        `,
+        variables() {
+          return {
+            searchString: this.$route.params.name,
+            searchField: 'USERNAME'
+          }
+        },
+        update: (data) => data.user
+      }
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
